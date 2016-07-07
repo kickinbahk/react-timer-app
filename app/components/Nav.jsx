5be29d4988bfc8dbc0c6cb57ec2bd891bdea50cd
kickinbahk <kickinbahk@gmail.com>
2016-07-07 08:40:55 -0700

    fix some typos and styles in nav

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,8 +1,7 @@
 var React = require('react')
-var {IndexLink} = require('react-router')
+var {Link, IndexLink} = require('react-router')
 
 var Nav = React.createClass({
-
   render: function () {
     return (
       <div className='top-bar'>
@@ -10,15 +9,18 @@ var Nav = React.createClass({
           <ul className='menu'>
             <li className='menu-text'>React Timer App</li>
             <li>
-              <IndexLink to='/'activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+              <IndexLink to='/' activeClassName='active-link' >Timer</IndexLink>
             </li>
             <li>
-              <IndexLink to='#' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Countdown</IndexLink>
+              <Link to='#' activeClassName='active-link'>Countdown</Link>
             </li>
           </ul>
         </div>
         <div className='top-bar-right'>
-          <p>Created by Josiah Mory</p>
+          <div className='menu'>
+            <p className='menu-text'>Created by Josiah Mory</p>
+          </div>
+
         </div>
       </div>
     )

fbfe3966d6193bb92cf7f24d035c187468cf2466
kickinbahk <kickinbahk@gmail.com>
2016-07-17 15:37:26 -0700

    add tests to check if controls exists

---
+++ app/tests/components/Controls.test.jsx
@@ -0,0 +1,13 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var expect = require('expect')
+var $ = require('jQuery')
+var TestUtils = require('react-addons-test-utils')
+
+var Controls = require('Controls')
+
+describe('Controls', function () {
+  it('should exist', function () {
+    expect(Controls).toExist()
+  })
+})

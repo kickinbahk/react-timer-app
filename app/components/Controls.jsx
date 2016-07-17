a1c5b037e2c3c5b45627498dde856c3679c997e9
kickinbahk <kickinbahk@gmail.com>
2016-07-17 15:26:15 -0700

    add controls file and test file

---
+++ app/components/Controls.jsx
@@ -0,0 +1,15 @@
+var React = require('react')
+
+var Controls = React.createClass({
+
+  render: function () {
+    return (
+      <div>
+        component
+      </div>
+    )
+  }
+
+})
+
+module.exports = Controls
